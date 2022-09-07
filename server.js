c6d7786fa0d073c95dd2958645fa26b990f78295
space_lion <rapture960@gmail.com>
2022-09-07 16:20:21 +0200

    beginning

---
+++ server.js
@@ -0,0 +1,8 @@
+const { Telegraf } = require('telegraf')
+
+const BOT_TOKEN = '5646501975:AAFZ37NppJplu4Ckgsk8iBs22gNNP-jVNDY'
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+
+bot.launch()
