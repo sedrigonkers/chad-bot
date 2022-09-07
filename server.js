c014e1b9c3ce11dfa0128df28ee940cb48901dea
space_lion <rapture960@gmail.com>
2022-09-07 16:23:45 +0200

    changed token problem

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { Telegraf } = require('telegraf')
 
 const BOT_TOKEN = '5646501975:AAFZ37NppJplu4Ckgsk8iBs22gNNP-jVNDY'
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(BOT_TOKEN)
 
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 
