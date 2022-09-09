701f2dc416f3805df4ad36dadf6cc2105688f460
space lion <rapture960@gmail.com>
2022-09-09 08:32:08 +0200

    interact with firebase

---
+++ firebase.js
@@ -0,0 +1,20 @@
+const { initializeApp } = require("firebase/app")
+const { getFirestore, collection, getDocs } = require("firebase/firestore")
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDL93kMJrzWo540eVYlqhosKVZU9Paod8w",
+    authDomain: "chad-bot-c5ae7.firebaseapp.com",
+    databaseURL: "https://chad-bot-c5ae7-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "chad-bot-c5ae7",
+    storageBucket: "chad-bot-c5ae7.appspot.com",
+    messagingSenderId: "98605001276",
+    appId: "1:98605001276:web:6f58ec8c7d1e0a48893139",
+    measurementId: "G-S0QPTXFD37"
+};
+
+const app = initializeApp(firebaseConfig);
+
+
+const db = getFirestore(app);
+
+module.exports = { db }
--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 const TelegramBot = require('node-telegram-bot-api')
+const { getFirestore, collection, getDocs, doc } = require("firebase/firestore")
+
+const { db } = require('./firebase')
+
+async function getCollection() {
+  const querySnapshot = await getDocs(collection(db, "motivation"));
+
+  querySnapshot.forEach((doc) => {
+    // console.log(`${doc.id} => ${doc.data().firstPhrase}`);
+    return doc.data().firstPhrase
+  });
+}
+
+getCollection()
 
 const token = '5646501975:AAFZ37NppJplu4Ckgsk8iBs22gNNP-jVNDY'
 
 const bot = new TelegramBot(token, { polling: true });
 
-bot.onText(/\/motivation/, (msg, match) => {
+bot.onText(/\/motivation/, async (msg) => {
 
   const chatId = msg.chat.id;
-  const resp = match[1];
 
-  bot.sendMessage(chatId, 'motivation phrase here');
-  console.log(match)
+  getDocs(collection(db, "motivation"))
+    .then(res => {
+      res.forEach((doc) => {
+        bot.sendMessage(chatId, doc.data().firstPhrase)
+      })
+    })
+
 });
 
 bot.on('edited_message', (msg) => {
